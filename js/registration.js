914f8abfc267c17831109346c05b49bc70539d00
Vikrant Gajria <vikrantgajria@gmail.com>
2019-05-01 19:42:53 +0530

    Prevent multiple submits

--- js/registration.js
+++ js/registration.js
@@ -1,4 +1,5 @@
 var regForm = document.getElementById('reg-form');
+var submitButton = document.getElementById('submitButton');
 
 let config = {
     apiKey: "AIzaSyC6-6aXNNf4OF4Ruwm3v9hFbJjkrfJhAK8",
@@ -10,8 +11,12 @@ let config = {
 };
 firebase.initializeApp(config);
 
+var lock = false; // prevent multiple submits.
+
 regForm.onsubmit = function(e) {
     e.preventDefault();
+
+    if(lock) return;
 
     let data = {};
     new FormData(regForm).forEach((value, key) => {data[key] = value;});
@@ -20,6 +25,10 @@ regForm.onsubmit = function(e) {
         alert("Preference 1 and 2 cannot be the same!");
         return;
     }
+
+    submitButton.classList.add('disabled');
+    submitButton.type = "";
+    lock = true;
 
     firebase.firestore()
         .collection('registrations')
@@ -33,5 +42,10 @@ regForm.onsubmit = function(e) {
             alert("There was a problem in registration. Please tell this to -> gajriavikrant@gmail.com.");
             console.warn("An error occured, contact Vikrant.");
             console.error(e);
+        })
+        .finally(() => {
+            submitButton.classList.remove('disabled');
+            submitButton.type = "submit";
+            lock = false;
         });
 };
